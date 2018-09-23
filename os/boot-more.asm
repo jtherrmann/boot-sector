ebd7dae4991cdc38bb425e5ce8409e7e67af40c5
Jake Herrmann <jtherrmann@alaska.edu>
2018-09-22 16:55:16 -0800

    Split boot-more.asm into two files.

--- os/boot-more.asm
+++ os/boot-more.asm
@@ -1,12 +1,14 @@
+	%include "boot-more1.asm"
+
 	bits 16
 
 	;; https://stackoverflow.com/q/4903906/10402025
-	org 0x7c00
+	;; org 0x7c00
 
-	;; mov ax, 0x7c0
-	;; mov ds, ax
-	mov ax, 0x0000
+	mov ax, 0x7c0
 	mov ds, ax
+	;; mov ax, 0x0000
+	;; mov ds, ax
 
 	jmp code
 
@@ -42,27 +44,3 @@ code:
 	times 510-($-$$) db 0 
 	db 0x55
 	db 0xaa
-
-nextSector:	
-
-	;; for int 0x10
-	mov ah, 0x0e
-
-	;; Try to print first char of nextStr (defined below).
-	mov di, nextStr
-	mov BYTE al, [di]
-	int 0x10	; should print B
-
-	;; Move 'C' into nextStr and print it.
-	mov BYTE [di], 'C'
-	mov BYTE al, [di]
-	int 0x10	; prints C
-
-	;; Print first char of bootStr again.
-	mov di, bootStr
-	mov BYTE al, [di]
-	int 0x10	; prints A
-
-	hlt
-
-	nextStr db "BBB"
